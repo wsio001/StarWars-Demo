3f59ff33e3ec38fab0fc0b14cf4db5ebe0c18022
William Sio <williamsio@williams-mbp.lan>
2020-08-31 15:46:54 -0400

    added message, and reset functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState,useRef} from 'react';
 import './App.css';
+import './Info.css';
 import Person from './Person';
 import Species from './Species';
 import Starships from './Starships';
@@ -8,10 +9,11 @@ import Films from './Films';
 const App = () =>{
 
   const firstInit = useRef(true);
+  let key = 0;
 
   const [people, setPeople] = useState(()=>{return []});
-  const [species, setSpecies] = useState(()=>{return []});
   const [speciesQ, setSpeciesQ] = useState(()=>{return []});
+  const [species, setSpecies] = useState(()=>{return []});
   const [starshipsQ, setStarshipsQ] = useState(()=>{return []});
   const [starships, setStarships] = useState(()=>{return []});
   const [filmsQ, setFilmsQ] = useState(()=>{return []});
@@ -19,6 +21,7 @@ const App = () =>{
   const [showSpButton, setShowSpButton] = useState(()=>{return false});
   const [showShipButton, setShowShipButton] = useState(()=>{return false});
   const [showFilmButton, setShowFilmButton] = useState(()=>{return false});
+  const [msg, setMsg] = useState(()=>{return ''});
   
 
   const [search, setSearch] = useState(() => {
@@ -26,16 +29,33 @@ const App = () =>{
     return '';
   });
 
+  const reset = () => {
+    setShowSpButton(false);
+    setShowShipButton(false);
+    setShowFilmButton(false);
+    setPeople([]);
+    setFilms([]);
+    setSpecies([])
+    setStarships([]);
+    setMsg('');
+  }
+
   const getObject = async (type, url) => {
-    
+
     const response = await fetch(url);
     const data = await response.json();
+
+    
+    if(data.count === 0){
+      setMsg("Sorry, we cannot find what you are looking for. May the force still be with you!")
+      return;
+    }
 
     switch(type){
       case 'people':
         setPeople(data.results);
-        setSpeciesQ(data.results[0].species);
         setFilmsQ(data.results[0].films);
+        setSpeciesQ(data.results[0].species)
         setStarshipsQ(data.results[0].starships);
         setShowSpButton(true);
         setShowShipButton(true);
@@ -51,26 +71,36 @@ const App = () =>{
         setFilms(previous => [...previous,data ]);
         break;
     }
-    return {data}
   };
 
   const updateSearch = e => {
+
     const searchTerm = e.target.value;
     setSearch(searchTerm);
     //console.log("evalue", searchTerm);
     //console.log("search that i just typed in", search);
   }
+  
 
   const getSearch = e =>{
     e.preventDefault(); //after hitting submit the page refresh, this prevent the page from refre
-    console.log("search term: ", search);
+    if (search === ''){
+      setMsg("Sorry, you will need to enter a name");
+      return;
+    }
     getObject('people','https://swapi.dev/api/people/?search=' + search);
+    reset();
     setSearch('');
   }
 
   const showSpecies = () =>{
     setShowSpButton(false);
-    getObject('species',speciesQ);
+    if(speciesQ.length !=0){
+      getObject('species',speciesQ);
+    }else{
+      key = key + 1;
+      setSpecies({key: key, name:'N\A'});
+    }
   }
 
   const showShips = () =>{
@@ -87,26 +117,38 @@ const App = () =>{
     }
   }
 
+
+  const showSpeciesSpace = showSpButton?'Show species':null;
+  const showShipsSpace = showShipButton?'Show ships':null;
+  const showFilmsSpace = showFilmButton?'Show Films':null;
+
   const extraContent = <div>
   <span>{starshipsQ}</span> <br/>
   <span>{filmsQ}</span> <br/>
   </div>
 
-  const showSpeciesSpace = showSpButton?'Show species':null;
-  const showShipsSpace = showShipButton?'Show ships':null;
-  const showFilmsSpace = showFilmButton?'Show Films':null;
-  //const showFilmsSpace = showButton?'Show films':null;
-
   return(
     <div className = "App">
+      <h1 className = 'Title' ><b>StarWars API Demo</b></h1>
       <form onSubmit = {getSearch} className = "search-form">
-        <input className = "search-bar" type = 'text' value = {search} onChange={updateSearch}/>
-        <button className = "search-button" type = 'submit'>
+        <input className = "search-bar" type="text" placeholder="Enter Character" value = {search} onChange={updateSearch} />
+        <button className = "search-button" type = 'submit' size = 'sm'>
           Search
         </button>
       </form>
+      <br/> 
+
+
+      <div className = "boxModel">
+        <div className = "Info">
+        {
+          msg.length>0?
+          msg:
+          null
+        }
+        </div>
         {people.map(person => (
-          <Person 
+          <Person
           key = {person.url} 
           name = {person.name}
           height = {person.height}
@@ -116,9 +158,9 @@ const App = () =>{
           />
         ))}
 
-        <a onClick = {showSpecies}>{showSpeciesSpace}</a><br/><br/><br/>
+        <p className = 'readMore' onClick = {showSpecies}>{showSpeciesSpace}</p>
         {
-          species.length > 0?
+          Object.keys(species).length?
           [species].map(s => (
             <Species 
             key = {s.url} 
@@ -128,9 +170,9 @@ const App = () =>{
           :null
         }
         
-        <a onClick = {showShips}>{showShipsSpace}</a><br/><br/><br/>
+        <p className = 'readMore' onClick = {showShips}>{showShipsSpace}</p>
         {
-          starships.length > 0?
+          starshipsQ.length > 0?
           starships.map(sh => (
             <Starships 
             key = {sh.url} 
@@ -140,9 +182,9 @@ const App = () =>{
           :null
         }
 
-        <a onClick = {showFilms}>{showFilmsSpace}</a>
+        <p className = 'readMore' onClick = {showFilms}>{showFilmsSpace}</p>
         {
-          films.length > 0?
+          filmsQ.length >0?
           films.map(f => (
             <Films 
             key = {f.url} 
@@ -151,7 +193,7 @@ const App = () =>{
           ))
           :null
         }
-
+      </div>
     </div>
   );
 };
